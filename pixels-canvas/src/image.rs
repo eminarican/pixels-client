83b7c267872734fd8e5a52caaf0f6507ed3ffcc1
Speretta <speretta@protonmail.com>
2023-03-04 23:28:17 +0300

    Updated Image

--- pixels-canvas/src/image.rs
+++ pixels-canvas/src/image.rs
@@ -14,7 +14,25 @@ pub struct Image{
 
 impl Image{
     pub fn new<P: AsRef<Path>>(path: P) -> Self{
-        todo!()
+        let image = image::io::Reader::open(path).unwrap().decode().unwrap();
+        let size = (image.width() as u64, image.height() as u64);
+
+        let mut y_axis = Vec::with_capacity(size.1 as usize);
+        let mut x_axis = Vec::with_capacity(size.0 as usize);
+
+        for (i, pixel) in image.as_rgb8().unwrap().pixels().into_iter().enumerate() {
+            if i % size.0 as usize == 0 && i >= size.0 as usize{
+                y_axis.push(x_axis.clone());
+                x_axis.clear()
+            }
+            let [r, g, b] = pixel.0;
+            x_axis.push(Color::from_rgb(r, g, b));
+        }
+        y_axis.push(x_axis);
+        Image {
+            data: y_axis,
+            size
+        }
     }
 
     pub fn from_vec(data: Vec<u8>, size: (u64, u64)) -> Self{
