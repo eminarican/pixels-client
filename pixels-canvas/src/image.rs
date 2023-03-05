295c586571745ee80f3e11bd11d597e6ce5e3a0c
Emin Arıcan <emin.arican@outlook.com>
2023-03-05 12:05:17 +0300

    simplification

--- pixels-canvas/src/image.rs
+++ pixels-canvas/src/image.rs
@@ -12,40 +12,29 @@ impl Image {
         let image = image::io::Reader::open(path).unwrap().decode().unwrap();
         let size = (image.width() as u64, image.height() as u64);
 
-        let mut y_axis = Vec::with_capacity(size.1 as usize);
-        let mut x_axis = Vec::with_capacity(size.0 as usize);
-
-        for (i, pixel) in image.as_rgb8().unwrap().pixels().into_iter().enumerate() {
-            if i % size.0 as usize == 0 && i >= size.0 as usize {
-                y_axis.push(x_axis.clone());
-                x_axis.clear()
-            }
-            let [r, g, b] = pixel.0;
-            x_axis.push(Color::from_rgb(r, g, b));
-        }
-        y_axis.push(x_axis);
-        Image {
-            data: y_axis,
-            size
-        }
+        Image::from_vec(image.into_bytes(), size)
     }
 
-    pub fn from_vec(data: Vec<u8>, size: (u64, u64)) -> Self{
-        let mut y_axis = Vec::with_capacity(size.1 as usize);
-        let mut x_axis = Vec::with_capacity(size.0 as usize);
+    pub fn from_vec(raw_data: Vec<u8>, size: (u64, u64)) -> Self {
+        let mut data = Vec::with_capacity(size.1 as usize);
+        let mut row = Vec::with_capacity(size.0 as usize);
 
-        for (i, raw_color) in data.chunks(3).enumerate(){
-            if let [r, g, b] = raw_color{
-                if i % size.0 as usize == 0 && i >= size.0 as usize{
-                    y_axis.push(x_axis.clone());
-                    x_axis.clear()
-                }
-                x_axis.push(Color::from_rgb(*r, *g, *b));
+        let row_size = size.0 as usize;
+
+        for chunk in raw_data.chunks_exact(3) {
+            row.push(Color::from_rgb(
+                chunk[0],
+                chunk[1],
+                chunk[2],
+            ));
+
+            if row.len() == row_size {
+                data.push(row.clone());
+                row.clear();
             }
-            
         }
-        y_axis.push(x_axis.clone());
-        Image { data: y_axis, size }
+
+        Image { data, size }
     }
 
     pub fn width(&self) -> u64 {
@@ -56,23 +45,36 @@ impl Image {
         self.size.1
     }
 
-    pub fn get_pixel_color(&self, x: usize, y: usize) -> Option<&Color>{
-        
+    pub fn get_pixel_color(&self, x: usize, y: usize) -> Option<&Color> {
         self.data.get(y).and_then(|x_axis| x_axis.get(x))
     }
 
-    pub fn set_pixel_color(&mut self, x: usize, y: usize, color: Color){
+    pub fn set_pixel_color(&mut self, x: usize, y: usize, color: Color) {
         if let Some(pixel) = self.data.get_mut(y).and_then(|x_axis| x_axis.get_mut(x)){
             *pixel = color;
         }
     }
 
-    pub fn replace_part_with_image(&mut self, part_location_x: usize, part_location_y: usize, part_image: &Image){
-        for (y, y_elements) in self.data.get_mut(part_location_y.min(self.size.1 as usize)..(part_location_y + part_image.size.1 as usize).min(self.size.1 as usize)).expect(format!("Unexpected location: (x: {}, y: {})", part_location_x, part_location_y).as_str()).into_iter().enumerate(){
-            for (x, color) in y_elements.get_mut(part_location_x.min(self.size.0 as usize)..(part_location_x + part_image.size.0 as usize).min(self.size.0 as usize)).expect(format!("Unexpected location: (x: {}, y: {})", part_location_x, part_location_y).as_str()).into_iter().enumerate(){
-                *color = *part_image.get_pixel_color(x, y).expect(format!("Unexpected location: (x: {}, y: {})", part_location_x, part_location_y).as_str());
+    pub fn replace_part_with_image(&mut self, part_x: usize, part_y: usize, image: &Image) {
+        let (width, height) = (
+            self.size.0 as usize,
+            self.size.1 as usize
+        );
+
+        let (x1, y1) = (
+            part_x.min(width),
+            part_y.min(height)
+        );
+
+        let (x2, y2) = (
+            (x1 + image.size.0 as usize).min(width),
+            (y1 + image.size.1 as usize).min(height)
+        );
+
+        for (y, row) in self.data.get_mut(y1..y2).unwrap().iter_mut().enumerate() {
+            for (x, pixel) in row.get_mut(x1..x2).unwrap().iter_mut().enumerate() {
+                *pixel = *image.get_pixel_color(x, y).unwrap();
             }
         }
     }
 }
-
